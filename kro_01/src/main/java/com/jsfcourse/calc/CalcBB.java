9914703d3be8ac55870362cc30b477537f868a86
Kubson79 <79503949+Kubson79@users.noreply.github.com>
2023-11-13 20:07:38 +0100

    Add files via upload

---
+++ kro_01/src/main/java/com/jsfcourse/calc/CalcBB.java
@@ -0,0 +1,88 @@
+package com.jsfcourse.calc;
+
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.enterprise.context.SessionScoped;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
+
+
+@Named
+@RequestScoped
+//@SessionScoped
+
+public class CalcBB {
+	
+	private String x;
+	private String y;
+	private Double result;
+
+	@Inject
+	FacesContext ctx;
+
+	public String getX() {
+		return x ;
+	}
+
+	public void setX(String x) {
+		this.x = x;
+	}
+
+	public String getY() {
+		return y ;
+	}
+
+	public void setY(String y) {
+		this.y = y;
+	}
+	
+	public Double getResult() {
+		return result;
+	}
+
+	public void setResult(Double result) {
+		this.result = result;
+	}
+	
+	
+	// Funkcja walidacyjna
+    public boolean sprawdzenie(String input) {
+        try {
+            Double.parseDouble(input);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+	
+	public boolean obliczRate() {
+		
+		if (!sprawdzenie(x)|| !sprawdzenie(y)) {
+		ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "BŁĘDNE DANE WEJSCIOWE PODAJ POPRAWNE WARTOSCI",null));
+		return false;
+		}	
+		
+		try {
+			double x = Double.parseDouble(this.x);
+			double y = Double.parseDouble(this.y);
+
+			result = (x / (y*12)) + ((x / (y*12))*0.23);
+
+			ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Operacja wykonana poprawnie", null));
+			return true;
+		} catch (Exception e) {
+			ctx.addMessage(null,
+					new FacesMessage(FacesMessage.SEVERITY_ERROR, "Blad podczas przetwarzania parametrów", null));
+			return false;
+		}
+	}
+
+	// Go to "showresult" if ok
+	public String calc() {
+		if (obliczRate()) {
+			return "showRata";
+		} 			
+		return null;
+	}
+}
